0074f6bf2d5595fa0aa4b1d25cb6385ab46876a8
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-04-08 23:57:59 +0300

    reduce example

--- main.cpp
+++ main.cpp
@@ -9,6 +9,6 @@ PYBIND11_MODULE(dev_pybind, m)
 {
   py::class_<Foo> (m,"Foo");
   using T = std::vector<Foo>;
-//  py::class_<T> (m,"T");
-  m.def("func", [](T* x) { }, py::arg("x").none(true) = static_cast<T*>(nullptr));
+
+  m.def("func", [](T* x) { });
 }
